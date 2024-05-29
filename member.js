34f8dca6e1679282d30d6ef1a5a68ffab8a95d44
Alexis Hernández <50474706+alexis11hm@users.noreply.github.com>
2024-05-29 17:07:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillMembers() {
+  return {
+    members: [
+      { id: 1, name: 'John', skills: ['HTML', 'CSS'] },
+      { id: 2, name: 'Jane', skills: ['JavaScript', 'React'] }
+    ]
+  };
+}
