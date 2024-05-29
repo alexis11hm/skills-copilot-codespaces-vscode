079c3c93f094717900d0151cee09619c0122630f
Alexis Hernández <50474706+alexis11hm@users.noreply.github.com>
2024-05-29 17:13:49 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,62 @@
+// Create web server
+// Create a web server that listens on port 3000. When a request is made to the server, respond with the message "Hello, World!".
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+// Path: comments.js
+// Create a web server that listens on port 3000. When a request is made to the server, respond with the message "Hello, World!".
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('Hello, World!');
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
+// Path: comments.js
+// Create a web server that listens on port 3000. When a request is made to the server, respond with the message "Hello, World!".
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('Hello, World!');
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
+// Path: comments.js
+// Create a web server that listens on port 3000. When a request is made to the server, respond with the message "Hello, World!".
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('Hello, World!');
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
+// Path: comments.js
+// Create a web server that listens on port 3000. When a request is made to the server, respond with the message "Hello, World!".
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('Hello, World!');
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
+// Path: comments.js
+// Create a web server that listens on port 3000. When a request is made to the server, respond with the message
